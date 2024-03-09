b1d828da14457d92e7d10c250fdd0d15bb0537e1
RanaAnwar1 <104921409+RanaAnwar1@users.noreply.github.com>
2024-03-09 22:19:03 +0200

    Update and rename main.c to SUFOTA.c

---
+++ SUFOTA.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello from SUFOTA!\n");
+    return 0;
+}
--- main.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-void main(){
-	printf("Hello From SUFOTA-Package\n");
-	return 0;
-}
